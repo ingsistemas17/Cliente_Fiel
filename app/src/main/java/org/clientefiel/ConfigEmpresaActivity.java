7cb6f8ceb1b04d050127dcd8a15316449da95c95
ingsistemas17 <tecsistemas17@gmail.com>
2016-09-24 14:15:21 -0500

    se agregan las primereas vistas actividades de la aplicacion

---
+++ app/src/main/java/org/clientefiel/ConfigEmpresaActivity.java
@@ -0,0 +1,16 @@
+package org.clientefiel;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by carlos.morales on 24/09/2016.
+ */
+public class ConfigEmpresaActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.config_empresa);
+    }
+}
