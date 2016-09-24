6a476521b8954b4427d32c8ab0b37e426f1b984f
ingsistemas17 <tecsistemas17@gmail.com>
2016-09-24 12:27:25 -0500

    se  agrega los paquetes del modelo, controlador,  globales, se configuran los colores de la pantallase cambio a verde.

---
+++ app/src/main/java/org/clientefiel/ListEmpresas.java
@@ -0,0 +1,16 @@
+package org.clientefiel;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by carlos.morales on 24/09/2016.
+ */
+public class ListEmpresas extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_empresas);
+    }
+}
--- app/src/main/java/org/clientefiel/LoginActivity.java
+++ app/src/main/java/org/clientefiel/LoginActivity.java
@@ -1,7 +1,9 @@
 package org.clientefiel;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by carlos.morales on 23/09/2016.
@@ -12,5 +14,15 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+    }
+
+    /**
+     * lanza la ventana para que el usuario se registre
+     * @param view
+     */
+    public void btn_onClick_Event (View view){
+        Intent i = new Intent(this, RegistroActivity.class);
+        startActivity(i);
+
     }
 }
