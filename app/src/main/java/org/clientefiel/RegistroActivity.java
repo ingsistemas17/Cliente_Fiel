37c118e262ef59449873026d58bb359a50ef2006
ingsistemas17 <tecsistemas17@gmail.com>
2016-09-23 23:49:48 -0500

    se agrega registro

---
+++ app/src/main/java/org/clientefiel/RegistroActivity.java
@@ -0,0 +1,16 @@
+package org.clientefiel;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by carlos.morales on 23/09/2016.
+ */
+public class RegistroActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registro);
+    }
+}
